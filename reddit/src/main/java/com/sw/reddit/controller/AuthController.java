f317672c1c393799b9bd2585f6a64573c922a790
sedx876 <swatkins876@gmail.com>
2021-05-19 07:46:23 -0400

    Added dto/RegisterRequest.java

---
+++ reddit/src/main/java/com/sw/reddit/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.sw.reddit.controller;
+
+import com.sw.reddit.dto.RegisterRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping("/api/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
+        authService.signup(registerRequest);
+        return new ResponseEntity<>("User Registration Successful",
+                OK);
+    }
+}
---
+++ reddit/src/main/java/com/sw/reddit/service/AuthService.java
@@ -0,0 +1,4 @@
+package com.sw.reddit.service;
+
+public class AuthService {
+}
